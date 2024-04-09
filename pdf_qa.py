d5b4d3413581480e2e2d9cc16f317a43d78074bd
naman-sfs <164490127+naman-sfs@users.noreply.github.com>
2024-04-09 16:53:24 +0530

    fifth

--- pdf_qa.py
+++ pdf_qa.py
@@ -14,7 +14,6 @@ from transformers import AutoTokenizer
 import torch
 import os
 import re
-import OpenAI
 from langchain.prompts import PromptTemplate
 
 os.environ["OPENAI_API_KEY"] = "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"
